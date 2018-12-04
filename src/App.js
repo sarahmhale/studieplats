6213de3c7f62c92e9ae806f1a419ae57e9e647f7
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-12-04 16:31:45 +0100

    Added diagram image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ZonesList from './ZonesList.js';
+import logo from './logo.svg'; 
 
 class App extends Component {
 
@@ -8,7 +9,11 @@ class App extends Component {
     return (
       <div className="App">
         <ZonesList/>
+        <div className="image-cover">
+          <img src={logo} className="best-image" alt=''/>
+        </div>
       </div>
+
     );
   }
 }
