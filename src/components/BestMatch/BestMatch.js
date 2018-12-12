db46e59374c1d201e5977fb9e2a9061098fd2ae5
Sarah <sarahhale95@gmail.com>
2018-12-12 11:45:34 +0100

    added text

--- src/components/BestMatch/BestMatch.js
+++ src/components/BestMatch/BestMatch.js
@@ -4,10 +4,10 @@ import Star from '../StarComponent/Star.js';
 
 export default class ListItem extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            style:{},
+            style: {},
         }
     }
 
@@ -15,27 +15,47 @@ export default class ListItem extends Component {
         let stars = []
 
         for (let j = 0; j < 50; j++) {
-            stars.push(<Star/>)
+            stars.push(<Star />)
         }
         return stars;
     }
 
-    expand(){
-        
+    expand() {
+
+    }
+
+    level(level) {
+        console.log(level)
+        switch (level) {
+            case 1:
+                return (<div><p>Really easy to find a seat</p></div>)
+                break
+            case 2:
+                return (<div><p>Easy to find a seat</p></div>)
+                break;
+            case 3:
+                return (<div><p>Hard to find a seat</p></div>)
+                break;
+            case 4:
+                return (<div><p>Really hard to find a seat</p></div>)
+                break;
+            default:
+
+        }
     }
 
     render() {
-
         return (
             <div className="best-container">
                 {this.createStars()}
                 <h3>Most seats available at</h3>
                 <h1>{this.props.zone.house}huset</h1>
                 <h2>Floor {this.props.zone.floor}</h2>
+                {this.level(this.props.zone.level)}
                 <div className="location-button" alt=''>
                     <p>Show me</p>
                 </div>
-                <div className="image-cover"/>
+                <div className="image-cover" />
             </div>
         );
     }
