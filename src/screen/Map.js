d4af243ef845d89feb60b032f6db32929a96a606
Sarah <sarahhale95@gmail.com>
2018-12-13 10:39:04 +0100

    map

--- src/screen/Map.js
+++ src/screen/Map.js
@@ -3,18 +3,21 @@ import Map from 'pigeon-maps'
 // import Marker from 'pigeon-marker'
 import Overlay from 'pigeon-overlay'
 
-import pigeon from './pigeon.jpg'
+import marker from './marker.png'
 
 export default class Demo extends Component {
     render() {
         return (
-            <div style={{ textAlign: 'center', marginTop: 50 }}>
-                <Map center={[50.879, 4.6997]}
-                    zoom={12}
+            <div style={{ textAlign: 'center', marginTop: 50, height: 100 }}>
+                <Map center={[63.819740, 20.307033]}
+                    zoom={16}
                     width={600}
                     height={400}>
-
+                    <Overlay anchor={[63.819740, 20.307033]} offset={[0, 0]}>
+                        <img src={marker} width={24} height={30} alt='' />
+                    </Overlay>
                 </Map>
+                <h1>Naturhuset våning 3</h1>
             </div>
         )
     }
